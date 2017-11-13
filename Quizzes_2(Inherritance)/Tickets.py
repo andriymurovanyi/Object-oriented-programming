f26a93478b09b0f0b5d42f1974b95b7d1b78c3ba
andriymurovanyi <andriymurovanyi@gmail.com>
2017-11-13 10:02:17 +0200

    Add files via upload

--- Quizzes_2(Inherritance)/Tickets.py
+++ Quizzes_2(Inherritance)/Tickets.py
@@ -17,28 +17,13 @@
 # Let's suppose, that the IT-event will be today.
 from datetime import datetime
 from uuid import uuid4
+import json
 
 
-class Categories:
-    """
-    Preferential categories.
-
-    This class include preferential categories and allows adding new category.
-    """
-    categories = {
-        'Pensioner': 0.3,
-        'Invalid': 0.35,
-        'Student': 0.5
-    }
-
-    def print_categories(self):
-        """
-        Printing list of categories.
-        """
-        a = ''
-        for i in self.categories.items():
-            a += str(i) + '\n'
-        return a
+def open_file():
+    with open('Data.json', 'r') as data:
+        f = json.load(data)
+    return f
 
 
 class Ticket:
@@ -47,53 +32,44 @@ class Ticket:
     """
 
     def __init__(self, unique_number=None,
-                 price=None, category=None):
+                 price=None, coefficient=None):
         """
         Constructor.
-
         Used to set a unique number and price.
         :param unique_number: unique ID of ticket
         :param price: price of ticket
         :param category: category
         """
-
-        if type(unique_number) == str and type(price) == float:
-            self._unique_number = unique_number
-            self._price = price
-            self._category = category
-        else:
-            raise TypeError
+        self._unique_number = unique_number
+        self._price = price
+        self._coefficient = coefficient
 
     def get_price(self):
         """
         Price.
-
         :return: a price of ticket
         """
-        self._price *= Categories.categories[self._category]
+        self._price *= self._coefficient
         return 'The price of current ticket is: ' + str(self._price)
 
     def ticket_as_string(self):
         """
         Information.
-
         :return: information about ticket in string-form.
         """
-        self._price *= Categories.categories[self._category]
+        self._price *= self._coefficient
         return 'Ticket number: {}\n' \
                'Ticket price: {}'.format(self._unique_number, self._price)
 
     def ticket_by_number(self):
         """
         Information.
-
         This method used to print information about ticket
         :return: Unique number, Price, Ticket name
         """
-        self._price *= Categories.categories[self._category]
+        self._price *= self._coefficient
         return 'Unique number: ' + self._unique_number + '\n' + \
-               'Price: ' + str(self._price) + '\n' + \
-               'Ticket name: ' + str(self.__class__.__name__)
+               'Price: ' + str(self._price) + '\n'
 
 
 class TicketDataBase(Ticket):
@@ -155,12 +131,21 @@ def main():
     A main-function, which allows to test this class
     """
     while True:
+        my_file = open_file()['IT-events']
+        for i in my_file:
+            print(i)
+        choice = input('Choose the IT-event in list higher: ')
+
         construct = input('Do you want add a new ticket ?[y/n]\n'
                           '>>>')
-        price = 100.0
+
+        price = my_file[choice]['price']
         unique_number = 'id' + str(uuid4().hex)
-        category = input('Choose a category, to which you belong: \n' +
-                         str(Categories().print_categories()))
+        print('There are following preferential categories on this event: ')
+        for i in my_file[choice]['categories']:
+            print('* ' + str(i))
+        category = input('Choose a category, to which you belong: ')
+        coefficient = my_file[choice]['categories'][category]
         if construct == 'y':
             unique_number_new = input('Choose a unique number for ticket: \n'
                                       '>>> {}'.format('id'))
@@ -168,7 +153,7 @@ def main():
                 print('This number already exist!\n')
                 continue
             else:
-                ticket = Ticket(unique_number_new, price, category)
+                ticket = Ticket(unique_number_new, price, coefficient)
                 TicketDataBase().add_to_base()
 
                 print('Your ticket successfully added! ')
@@ -176,44 +161,37 @@ def main():
             if see == 'y':
                 print(ticket.ticket_by_number())
         elif construct == 'n':
-            try:
-                a = input('Please, input a date of event'
-                          ' in [dd/mm/yy] format: \n'
-                          '>>> ').split('/')
-                date_of_event = datetime.date(datetime(int(a[2]),
-                                                       int(a[1]), int(a[0])))
+            f = my_file[choice]['date']
+            date_of_event = datetime.date(datetime.strptime(f, '%d.%m.%y'))
+            print(date_of_event)
+            date = input('Input a date, when you bought a '
+                         'ticket in [dd.mm.yy] format: \n'
+                         '>>> ').split('.')
+            day, month, year = int(date[0]), int(date[1]), int(date[2])
 
-                date = input('Input a date, when you bought a '
-                             'ticket in [dd/mm/yy] format: \n'
-                             '>>> ').split('/')
-                day, month, year = int(date[0]), int(date[1]), int(date[2])
+            date_of_purchase = datetime.date(datetime(year, month, day))
 
-                date_of_purchase = datetime.date(datetime(year, month, day))
-            except ValueError:
-                print('Date is incorrect!')
-                continue
+            day_delta = str(date_of_event -
+                            date_of_purchase).split()
+            # difference between day of event
+            # and day, when ticket
+            # was purchased .
+
+            if int(day_delta[0]) >= 60:
+                ticket = Advanced(unique_number, price, coefficient)
+            elif int(day_delta[0]) <= 10:
+                ticket = Late(unique_number, price, coefficient)
+
+            action = input('What you want to do? (see a list below)\n'
+                           '<1 - ask a ticket price>\n'
+                           '<2 - print ticket as string>\n'
+                           '>>> ')
+            if action == '1':
+                print(ticket.get_price())
+            elif action == '2':
+                print(ticket.ticket_as_string())
             else:
-                day_delta = str(date_of_event -
-                                date_of_purchase).split()
-                # difference between day of event
-                # and day, when ticket
-                # was purchased .
-
-                if int(day_delta[0]) >= 60:
-                    ticket = Advanced(unique_number, price, category)
-                elif int(day_delta[0]) <= 10:
-                    ticket = Late(unique_number, price, category)
-
-                action = input('What you want to do? (see a list below)\n'
-                               '<1 - ask a ticket price>\n'
-                               '<2 - print ticket as string>\n'
-                               '>>> ')
-                if action == '1':
-                    print(ticket.get_price())
-                elif action == '2':
-                    print(ticket.ticket_as_string())
-                else:
-                    print('Please, make you\'re choice')
+                print('Please, make you\'re choice')
 
         if input('Press <Enter> to continue work with program...') != '':
             break
